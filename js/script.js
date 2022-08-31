6efd2e6500187c9956f5ad2bca685261736e5cc3
Naggenda <naggendajoshua@gmail.com>
2022-08-31 10:31:02 +0300

    next and prev btns

--- js/script.js
+++ js/script.js
@@ -1,97 +1,129 @@
-let count = 0
+let count = 0;
 let totalPrice;
-let img = document.getElementById('img');
-let img1 = document.getElementById('img1'); 
-let img2 = document.getElementById('img2'); 
-let img3 = document.getElementById('img3'); 
+let img = document.getElementById("img");
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
 
-let navLinks = document.getElementById('nav-links');
-let countVl = document.getElementById('count-vl');
-let countEl = document.getElementById('count-el');
-let myCart = document.getElementById('my-cart');
-let itemPrice = document.getElementById('item-price')
+let navLinks = document.getElementById("nav-links");
+let countVl = document.getElementById("count-vl");
+let countEl = document.getElementById("count-el");
+let myCart = document.getElementById("my-cart");
+let itemPrice = document.getElementById("item-price");
 let price = 250;
 
-function toggle(){
-    if(navLinks.style.display=='none'){
-        navLinks.style.display="block";
-    }
-    else{
-        navLinks.style.display="none";
-    }
+function toggle() {
+  if (navLinks.style.display == "none") {
+    navLinks.style.display = "block";
+  } else {
+    navLinks.style.display = "none";
+  }
 }
 
-function decrement(){
-    count = count - 1;
-    countVl.innerText = count;
-    console.log(countVl);
-    
+function decrement() {
+  count = count - 1;
+  countVl.innerText = count;
+  console.log(countVl);
 }
 
-function increment(){
-    count += 1;
-    countVl.innerText = count;
+function increment() {
+  count += 1;
+  countVl.innerText = count;
 }
 
-function save(){
-    countEl.innerText = count;
-    totalPrice = count * price;
-    itemPrice.innerText += totalPrice;
+function save() {
+  countEl.innerText = count;
+  totalPrice = count * price;
+  itemPrice.innerText += totalPrice;
 }
 
-function cartItem(){
-    if(myCart.style.display == 'none'){
-        myCart.style.display = "block";
-    }
-    else{
-        myCart.style.display = "none";
-    }
-    
+function cartItem() {
+  if (myCart.style.display == "none") {
+    myCart.style.display = "block";
+  } else {
+    myCart.style.display = "none";
+  }
 }
 // image rendering
-function myfunction1(){
-    let img = document.getElementById('img');
-    // var p = document.getElementById('img1');
-    if(img1.style.display=='none'){
-        img1.style.display='block';
-    }
-    else{
-        img1.style.display='block';
-    }
-    
+function myfunction1() {
+  img1.style.display = "block";
+  img.style.display = "none";
+  img2.style.display = "none";
+  img3.style.display = "none";
 }
 
-function myfunction3(){
-    // let img = document.getElementById('img');
-    var img3 = document.getElementById('img3');
-    if(img3.style.display=='none'){
-        img3.style.display='block';
-    }
-    else{
-        img3.style.display='block';
-    }
-    
+function myfunction3() {
+  img3.style.display = "block";
+  img.style.display = "none";
+  img2.style.display = "none";
+  img1.style.display = "none";
 }
 
-function myfunction2(){
-    // let img = document.getElementById('img');
-    let img2 = document.getElementById('img2');
-    if(img2.style.display=='none'){
-        img2.style.display='block';
-    }
-    else{
-        img2.style.display='block';
-    }
-    
+function myfunction2() {
+  img2.style.display = "block";
+  img.style.display = "none";
+  img3.style.display = "none";
+  img1.style.display = "none";
 }
 
-function myfunction(){
-    let img = document.getElementById('img');
-    if(img.style.display=='none'){
-        img.style.display='block';
-    }
-    else{
-        img.style.display='block';
-    }
-    
+function myfunction() {
+  img.style.display = "block";
+  img3.style.display = "none";
+  img2.style.display = "none";
+  img1.style.display = "none";
+}
+
+// media query next and prev buttons
+function prevbtn() {
+  if (img.style.display == "block") {
+    img3.style.display = "block";
+    img2.style.display = "none";
+    img1.style.display = "none";
+    img.style.display = "none";
+  } 
+  else if (img3.style.display == "block") {
+    img2.style.display = "block";
+    img3.style.display = "none";
+    img1.style.display = "none";
+    img.style.display = "none";
+  }
+  else if (img2.style.display == "block") {
+    img1.style.display = "block";
+    img3.style.display = "none";
+    img2.style.display = "none";
+    img.style.display = "none";
+  }
+  else{
+    img.style.display = "block";
+    img3.style.display = "none";
+    img1.style.display = "none";
+    img2.style.display = "none";
+  }
+}
+
+function nextbtn() {
+    if (img.style.display == "block") {
+        img1.style.display = "block";
+        img2.style.display = "none";
+        img3.style.display = "none";
+        img.style.display = "none";
+      } 
+      else if (img1.style.display == "block") {
+        img2.style.display = "block";
+        img3.style.display = "none";
+        img1.style.display = "none";
+        img.style.display = "none";
+      }
+      else if (img2.style.display == "block") {
+        img3.style.display = "block";
+        img1.style.display = "none";
+        img2.style.display = "none";
+        img.style.display = "none";
+      }
+      else{
+        img.style.display = "block";
+        img3.style.display = "none";
+        img1.style.display = "none";
+        img2.style.display = "none";
+      }
 }
